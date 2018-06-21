072559f157b2541f50e9dfb1034d26f2b33d3b29
sangwon <sang10518@gmail.com>
2018-06-22 06:34:21 +0900

    Add function type check

--- hw2_json_func_call/index.js
+++ hw2_json_func_call/index.js
@@ -1,17 +1,24 @@
 let jsonData =  require('./function.json')
 
-const functionName = jsonData.function
+let functionName = jsonData.function
 
 const executeFuncInFile = function(func){
-    eval(func)()
+    const check = eval( 'typeof ' + func )
+    if ( check == 'function' ) {
+        eval(func)()
+    }
+    else {
+        console.log(`${func} is undefined as a function`);
+    }
 }
 
 const callTest = function (){
     console.log('callTest called')
 };
 
+const callTestInt = 1
+
 console.log(`function name is ${functionName}`)
 
 // executeFuncInFile(callTest)
 executeFuncInFile(functionName)
-
